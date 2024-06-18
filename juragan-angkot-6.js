54b2b982944bf4ef34ffaf8787c675a5c6928770
yojesstore <yojesstore@gmail.com>
2024-06-18 16:26:09 +0000

    membuat file juragan-angkot-6.js dan menambahkan materi

--- juragan-angkot-6.js
+++ juragan-angkot-6.js
@@ -2,12 +2,17 @@ var angkotBeroprasi = 6;
 var jmlhAngkot = 10;
 for( var angkot = 1; angkot <= jmlhAngkot; angkot++){
 
-    if(angkot <= angkotBeroprasi){
+    if(angkot <= angkotBeroprasi && angkot !== 5){
         console.log('angkot no '+angkot+' beroprasi dengan baik');
-    }else if (angkot === 8 || angkot === 9){
+    }else if(angkot === 6){
+        console.log('angkot no '+angkot+' beroprasi dengan baik');
+    }else if (angkot === 8 || angkot === 9 || angkot === 5){
         console.log('angkot no '+angkot+' sedang lembur');
     }else {
         console.log('angkot no '+angkot+' sedang tidak beroprasi');
     } 
     
 }
+
+// && / dan semua kondisi harus bernilai true
+//  || / or salah satu kondisi harus bernilai false
