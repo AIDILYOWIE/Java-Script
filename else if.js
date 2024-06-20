05d2c441a2cf814c31c16b0b952216dd773194c1
yojesstore <yojesstore@gmail.com>
2024-06-20 13:28:45 +0000

    menambahkan materi percabangan else if

--- else if.js
+++ else if.js
@@ -7,12 +7,23 @@
 // }
 
 
-var angka = prompt('masukan angka untuk penjumlahan = ');
+// var angka = prompt('masukan angka untuk penjumlahan = ');
 
-if(angka % 2 == 0){
-    alert(angka + ' adalah bilangan genap');
-}else if (angka % 2 == 1){
-    alert(angka +' adalah bilangan ganjil');
+// if(angka % 2 == 0){
+//     alert(angka + ' adalah bilangan genap');
+// }else if (angka % 2 == 1){
+//     alert(angka +' adalah bilangan ganjil');
+// }else {
+//     alert('yang anda masukan bukan angka !!!')
+// }
+
+// parseInt= untuk mengganti nilai data, yang awal nya string menjadi intejer
+var angka = parseInt(prompt('masukan angka anda : '));
+
+if( angka === 1){
+    alert('anda memasukan angka 1');
+}else if ( angka === 2){
+    alert('anda memasukan angka 2');
 }else {
-    alert('yang anda masukan bukan angka !!!')
+    alert ('anda memasukan angka salah ');
 }
