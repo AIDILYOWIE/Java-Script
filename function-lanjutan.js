7e0c4fff128789556ba61d10503a2765f52f0846
yojesstore <yojesstore@gmail.com>
2024-07-03 07:33:09 +0000

    menambahkan materi function lanjutan

---
+++ function-lanjutan.js
@@ -0,0 +1,36 @@
+// function declaration 
+var a = prompt('masukan nama anda ');
+var b = prompt('masukan status anda');
+Akusayang(a, b);
+function Akusayang(nama, status){
+    if (status == 'jomblo') {
+        alert('halo '+nama+' yahahahaa jomblo');
+    }else {
+        alert('halo '+nama+', status anda '+status);
+    }
+}
+
+
+
+// function expresion
+// var a = prompt('masukan nama anda');
+// var b = prompt('masukan status anda');
+// var c = function (nama, status){
+   
+//     if (status == 'jomblo'){
+//         console.log('halo '+nama+' yahhahha masih '+status);
+//     }else {
+//         console.log('halo '+nama+' status anda ternyata '+status);  
+//     }
+// }
+// c(a, b);
+// function bisa disebut expresion jika function tersebut di simpan di dalam varia
+
+// kita bisa memnggil dulu baru mendeklarasikan function
+
+// tampil('aidil');
+
+// function tampil(nama){
+//     console.log('halo '+nama)
+// }
+// untuk function expresion kita tida bisa panggil dulu, harus di definisikan terlebih dahulu
