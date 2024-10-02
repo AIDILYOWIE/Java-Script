9d913f4e13f42ab1dca08de653e01ff221c8364c
yojesstore <yojesstore@gmail.com>
2024-10-02 07:09:36 +0700

    materi Template Literal

---
+++ JS-lanjutan/templateLiteral.js
@@ -0,0 +1,30 @@
+// TEMPLATE LITERAL
+
+//  Multi-Line String
+console.log(`String 1  
+String 2`);
+// HTML FRAGMENTS,
+let mhs = {
+  nama: "aidil",
+  nomer: 18,
+  nrp: "95860459865"
+};
+let i = `<div class="mhs">
+            <h2>${mhs.nama}</h2>
+            <span class="nrp">${mhs.nrp}</span>
+        </div>`;
+console.log(i);
+
+// Embedde Expression
+let nama = "melisa";
+let umur = 17;
+console.log(`halo nama saya ${nama}, saya umur ${umur}`);
+
+// Expression Interpolation
+let a = 20;
+let b = 5;
+console.log(`total penjumlahan ${a + b}`);
+
+// html fragments
+
+
